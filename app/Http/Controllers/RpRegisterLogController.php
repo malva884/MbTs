b0ccb55201092cb797b81d92a48159b333ccc899
malva884 <gregorio.grande1984@gmail.com>
2024-03-19 23:25:43 +0100

    Calendar

--- app/Http/Controllers/RpRegisterLogController.php
+++ app/Http/Controllers/RpRegisterLogController.php
@@ -10,13 +10,14 @@ class RpRegisterLogController extends Controller
 {
     public function getRegister($id){
 
-        Log::channel('stderr')->info($id);
-        $obj = DB::table('rp_register_logs')->select('nome')->where('cod_riferimento',$id)
+
+        $obj = DB::table('rp_register_logs')->select('nome')
+            ->where('cod_riferimento','385a1bc1-4f39-4cae-9607-8520edb26bc2')
             ->where('data_scadenza','>', date('Y-m-d H:i:s'))
-            ->where('attivo','<>', true)
-            ->first();
+            //->where('attivo','<>', true)
+            ->get();
 
-
+        //Log::channel('stderr')->info($obj);
         return response()->json($obj);
     }
     public function register($id){

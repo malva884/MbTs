291e23d969978d1e43b0ad806cbbb6c66297284f
gregorio <gregorio.grande1984@gmail.com>
2026-06-21 16:06:28 +0200

    start

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: '/build/',
   plugins: [// Docs: https://github.com/posva/unplugin-vue-router
   // ℹ️ This plugin should be placed before vue plugin
     VueRouter({

b765ecec95ba87a82e5325f8a17157de080e7994
gregorio <gregorio.grande1984@gmail.com>
2026-09-21 12:24:04 +0200

    Bug Moviemnti di magazino

--- app/Console/Commands/InventoryAdjustment.php
+++ app/Console/Commands/InventoryAdjustment.php
@@ -39,8 +39,21 @@ class InventoryAdjustment extends Command
         $time = strtotime(date('Y-m-d').' -1 Day');
         $andDate = date('Y-m-d', $time);
         $objs = DB::table('pr_movements')
-            ->select('materiale','descrizione','quantita','importo','um',
-                DB::raw("CASE WHEN user = '23910730' THEN 'Sunpreet Singh' ELSE 'Sunpreet Singh' END as fullname"))
+            ->select('materiale','descrizione','quantita','importo','um','user',
+                DB::raw("CASE user
+                    WHEN '23920632' THEN 'Ghidin Roberta'
+                    WHEN '23910700' THEN 'Varisco Francesca'
+                    WHEN '23910519' THEN 'Busetti Daniela'
+                    WHEN '23910470' THEN 'Vignoni Davide'
+                    WHEN '23910430' THEN 'Guerreschi Antonio'
+                    WHEN '23910263' THEN 'Betella Gloria'
+                    WHEN '23920511' THEN 'Fogliata Vanni'
+                    WHEN '23920619' THEN 'Carrera Chiara'
+                    WHEN '23920599' THEN 'Vitarelli Gianpaolo'
+                    WHEN '23910730' THEN 'Singh Sunpreet'
+                    WHEN '23910839' THEN 'Ricca Asia'
+                    WHEN '23920682' THEN 'Roberta Cossetti'
+                    ELSE user END as fullname"))
             ->whereBetween('data_documento',[$stratDate,$andDate])
             ->whereIn('tipo_movimento',[701,702,201,202])
             ->orderBy('data_documento','asc')
